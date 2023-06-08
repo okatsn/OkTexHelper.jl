4db9fe851ea4ff258f8b46a1b0e12865e3157f85
Documenter.jl <documenter@juliadocs.github.io>
2023-06-08 06:31:40 +0000

    build based on c2f4170

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = OkTexHelper","category":"page"},{"location":"#OkTexHelper","page":"Home","title":"OkTexHelper","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for OkTexHelper.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [OkTexHelper]","category":"page"}]
+}
